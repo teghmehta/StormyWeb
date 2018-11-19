33a14f85548a556dc8d6a50b4374d5f7534a1701
Tegh <tghsm-adm@yahoo.ca>
2018-11-18 19:08:19 -0500

    CORS

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)).use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
