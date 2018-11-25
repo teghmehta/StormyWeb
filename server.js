2ee3cf44296123f4bb5a4f9d3828d5d742adb83c
Tegh <tghsm-adm@yahoo.ca>
2018-11-25 18:19:24 -0500

    quick fix

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+var cors = require('cors')
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname));;
+
+// use it before all route definitions
+app.use(cors({origin: '*'}));
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://api.darksky.net/forecast/9bb1e47fd64a2be700f417b9a7bf52e4/43.6942156,-79.25638049999999?units=auto');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
