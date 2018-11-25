7cdf967cc37a7c94132de2e2c09ff89577c42075
Tegh <tghsm-adm@yahoo.ca>
2018-11-25 18:24:19 -0500

    quick fix

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require('express');
-var cors = require('cors')
+let cors = require('cors')
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));;
-
-// use it before all route definitions
-app.use(cors({origin: '*'}));
-app.use(function(req, res, next) {
+app.use(express.static(__dirname))
+    .use(cors({origin: '*'}))
+    .use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+        // Pass to next layer of middleware
+        next();
 });
 
 
